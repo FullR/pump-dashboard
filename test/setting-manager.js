b92f147a6a8154b1b92bae6621d6ce0298101151
James Meyers <james.meyers919@gmail.com>
2015-10-21 12:24:56 -0700

    Fixed setting-manager test

--- test/setting-manager.js
+++ test/setting-manager.js
@@ -21,13 +21,13 @@ describe("SettingManager", () => {
     setTimeout(() => done(new Error("SettingManager did not emit a change event when modified")), 1000);
   });
 
-  it("should not emit a network-change event when replacing a network value with an identical value", () => {
+  it("should not emit a network-change event when replacing a network value with an identical value", (done) => {
     const settingManager = new SettingManager({address: [1, 1, 1, 1]});
 
     settingManager.once("network-change", () => done(new Error("emitted a network-change event despite the new value being identical to the old value")));
     settingManager.set({
       address: [1,1,1,1]
     });
-    setTimeout(() => done(new Error()), 300);
+    setTimeout(() => done(), 300);
   });
 });
