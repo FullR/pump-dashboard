6428a3e004a2a610c82e21aa18113b7e1368e42b
James Meyers <James.Meyers919@gmail.com>
2016-09-15 12:30:14 -0700

    Added gpio repo, js module, and build script

---
+++ bbb-gpio-util/index.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// #define GPIO_PATH "/home/james/projects/pump-dashboard/bbb-gpio-util/test-gpio"
+#define GPIO_PATH "/sys/class/gpio"
+#define EXPORT_PATH GPIO_PATH "/export"
+#define UNEXPORT_PATH GPIO_PATH "/unexport"
+
+char* validDirections[] = {"in", "out", "high", "low"};
+
+char pinPathBuffer[100];
+char pinValuePathBuffer[100];
+char pinDirPathBuffer[100];
+
+// returns true if error
+bool quickWriteFile(char* filename, char* data, size_t dataTypeSize, size_t dataLength) {
+  FILE* file = fopen(filename, "w");
+  size_t writtenLength = fwrite(data, dataTypeSize, dataLength, file);
+  fclose(file);
+  return dataLength != writtenLength;
+}
+
+bool strEq(char* a, char* b) {
+  return strcmp(a, b) == 0;
+}
+
+bool fileExists(char* filename) {
+  FILE* file = fopen(filename, "r");
+  if(file != NULL) {
+    fclose(file);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+char* getPinDirPath(char* pin) {
+  strcpy(pinDirPathBuffer, GPIO_PATH);
+  strcat(pinDirPathBuffer, "/");
+  strcat(pinDirPathBuffer, "gpio");
+  strcat(pinDirPathBuffer, pin);
+  strcat(pinDirPathBuffer, "/");
+  strcat(pinDirPathBuffer, "direction");
+  return pinDirPathBuffer;
+}
+
+char* getPinValuePath(char* pin) {
+  strcpy(pinValuePathBuffer, GPIO_PATH);
+  strcat(pinValuePathBuffer, "/");
+  strcat(pinValuePathBuffer, "gpio");
+  strcat(pinValuePathBuffer, pin);
+  strcat(pinValuePathBuffer, "/");
+  strcat(pinValuePathBuffer, "value");
+  return pinValuePathBuffer;
+}
+
+bool isValidDirection(char* direction) {
+  size_t i;
+  size_t length = sizeof(validDirections) / sizeof(char*);
+  for(i = 0; i < length; i++) {
+    if(strEq(direction, validDirections[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isValidValue(char* value) {
+  return strEq(value, "0") || strEq(value, "1");
+}
+
+void printUsage(char* command) {
+  printf("Usage:\n");
+  printf("  %s (pinId) (command) [commandArg]\n", command);
+  printf("  Commands:\n");
+  printf("    export - Export the given pin to make it available for IO\n");
+  printf("    unexport - Unexport the given pin to disable it for IO\n");
+  printf("    set-direction - Set the direction for a pin (valid values: high, low, up, down)\n");
+  printf("    set-value - Set the value for a pin (valid values: 1, 0)\n");
+}
+
+bool exportPin(char* pin) {
+  printf("Exporting %s\n", pin);
+  bool failed = quickWriteFile(EXPORT_PATH, pin, sizeof(char), strlen(pin));
+  if(failed) {
+    printf("Failed to export\n");
+    return true;
+  }
+  return false;
+}
+
+bool unexportPin(char* pin) {
+  printf("Unexporting %s\n", pin);
+  bool failed = quickWriteFile(UNEXPORT_PATH, pin, sizeof(char), strlen(pin));
+  if(failed) {
+    printf("Failed to unexport\n");
+    return true;
+  }
+  return false;
+}
+
+bool setDirection(char* pin, char* direction) {
+  if(!isValidDirection(direction)) {
+    printf("Invalid direction: %s\n", direction);
+    return true;
+  }
+  printf("Setting direction of pin %s to %s\n", pin, direction);
+  char* directionFileName = getPinDirPath(pin);
+
+  if(fileExists(directionFileName)) {
+    bool failed = quickWriteFile(directionFileName, direction, sizeof(char), strlen(direction));
+    if(failed) {
+      printf("Failed to write direction\n");
+      return true;
+    }
+  } else {
+    printf("Direction file not found. Was the pin exported?\n");
+    return true;
+  }
+  return false;
+}
+
+bool setValue(char* pin, char* value) {
+  if(!isValidValue(value)) {
+    printf("Invalid value: %s\n", value);
+    return true;
+  }
+  printf("Setting value of pin %s to %s\n", pin, value);
+  char* valueFileName = getPinValuePath(pin);
+  if(fileExists(valueFileName)) {
+    bool failed = quickWriteFile(valueFileName, value, sizeof(char), strlen(value));
+    if(failed) {
+      printf("Failed to write value\n");
+      return true;
+    }
+  } else {
+    printf("Value file not found. Was the pin exported?\n");
+    return true;
+  }
+  return false;
+}
+
+int main(size_t argc, char* argv[]) {
+  // printf("export path = %s\nunexport path = %s\n", EXPORT_PATH, UNEXPORT_PATH);
+  bool errorFlag;
+
+  if(argc <= 2 || strEq("--help", argv[1])) {
+    printUsage(argv[0]);
+  } else {
+    char* pin = argv[1];
+    char* command = argv[2];
+
+    if(strEq(command, "export")) {
+      errorFlag = exportPin(pin);
+    } else if(strEq(command, "unexport")) {
+      errorFlag = unexportPin(pin);
+    } else if(strEq(command, "set-direction")) {
+      if(argc <= 3) {
+        printf("set-direction requires direction argument\n");
+        errorFlag = true;
+      } else {
+        char* direction = argv[3];
+        errorFlag = setDirection(pin, direction);
+      }
+    } else if(strEq(command, "set-value")) {
+      if(argc <= 3) {
+        printf("set-value requires value argument\n");
+        errorFlag = true;
+      } else {
+        char* value = argv[3];
+        errorFlag = setValue(pin, value);
+      }
+    } else {
+      errorFlag = true;
+      printf("Unrecognized command: %s\n", command);
+      printUsage(argv[0]);
+    }
+  }
+
+  if(errorFlag) return 1;
+  return 0;
+}
---
+++ server/gpio.js
@@ -0,0 +1,124 @@
+const fs = require("fs");
+const exec = require("child_process").exec;
+const readFile = fs.readFile;
+const stat = fs.stat;
+const validDirections = ["high", "low", "in", "out"];
+const gpioExe = __dirname + "/gpio.exe";
+
+function fileExists(filename) {
+  return new Promise((resolve) => {
+    stat(filename, (error) => {
+      if(error) resolve(false);
+      else resolve(true);
+    });
+  });
+}
+
+function pinDir(pinId) {
+  return `/sys/class/gpio/gpio${pinId}`;
+}
+
+function pinFile(pinId, filename) {
+  return `${pinDir(pinId)}/${filename}`;
+}
+
+function readPinFile(pinId, filename) {
+  return new Promise((resolve, reject) => {
+    readFile(pinFile(pinId, filename), (error, result) => {
+      if(error) {
+        reject(error);
+      } else {
+        resolve(result.toString());
+      }
+    });
+  });
+}
+
+function runPinUtil(pinId, command, arg) {
+  return new Promise((resolve, reject) => {
+    exec(`${gpioExe} ${pinId} ${command} ${arg}`, (error, stdout, stderr) => {
+      if(error) {
+        reject(error);
+      } else {
+        resolve({stdout: stdout, stderr: stderr});
+      }
+    });
+  });
+}
+
+function setPinValue(pinId, value) {
+  return runPinUtil(pinId, "set-value", value ? "1" : "0");
+}
+
+function setPinDirection(pinId, direction) {
+  if(validDirections.indexOf(direction) === -1) throw new Error(`Invalid pin direction: ${direction}`);
+  return runPinUtil(pinId, "set-direction", direction);
+}
+
+function setPinActiveLow(pinId, value) {
+  return runPinUtil(pinId, "set-active-low", value ? "1" : "0");
+}
+
+function exportPin(pinId) {
+  return runPinUtil(pinId, "export");
+}
+
+function unexportPin(pinId) {
+  return runPinUtil(pinId, "unexport");
+}
+
+function isPinExported(pinId) {
+  return fileExists(pinDir(pinId));
+}
+
+function getPinValue(pinId) {
+  return readPinFile(pinId, "value")
+    .then((result) => parseInt(result));
+}
+
+function watchPin(pinId, listener) {
+  var timeout;
+  var prevValue = null;
+
+  function loop() {
+    getPinValue(pinId).then((result) => {
+      if(result !== prevValue) {
+        listener(result, prevValue);
+        prevValue = result;
+      }
+      timeout = setTimeout(loop, 100);
+    });
+  }
+
+  loop();
+  return () => clearTimeout(timeout);
+}
+
+function setupPin(pinId, direction) {
+  return isPinExported(pinId)
+    .then((isExported) => isExported ? unexportPin(pinId) : Promise.resolve())
+    .then(() => exportPin(pinId))
+    .then(() => setPinDirection(pinId, direction));
+}
+
+function setupOutputPin(pinId, value) {
+  return setupPin(pinId, value ? "high" : "low");
+}
+
+function setupInputPin(pinId, activeLow) {
+  return setupPin(pinId, "in")
+    .then(() => setPinActiveLow(pinId, activeLow));
+}
+
+module.exports = {
+  exportPin: exportPin,
+  unexportPin: unexportPin,
+  setPinValue: setPinValue,
+  setPinDirection: setPinDirection,
+  setPinActiveLow: setPinActiveLow,
+  getPinValue: getPinValue,
+  watchPin: watchPin,
+  setupPin: setupPin,
+  setupOutputPin: setupOutputPin,
+  setupInputPin: setupInputPin
+};
